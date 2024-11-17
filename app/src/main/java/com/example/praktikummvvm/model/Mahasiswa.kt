8b5b5665ada23bf451b0c404de38c8d5d078998b
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-11-17 17:21:35 +0700

    Menambahkan nim, email, noHp untuk data Mahasiswa

--- app/src/main/java/com/example/praktikummvvm/model/Mahasiswa.kt
+++ app/src/main/java/com/example/praktikummvvm/model/Mahasiswa.kt
@@ -1,7 +1,10 @@
 package com.example.praktikummvvm.model
 
 data class Mahasiswa(
+    val nim : String = "",
     val nama : String = "",
     val gender : String = "",
-    val alamat : String = ""
+    val alamat : String = "",
+    val email : String = "",
+    val noHp : String = ""
 )
