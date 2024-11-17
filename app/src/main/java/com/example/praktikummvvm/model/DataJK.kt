3def4b0ba00c9a4fa14a557ef0b0244ae0793189
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-11-17 14:47:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktikummvvm/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.praktikummvvm.model
+
+import com.example.praktikummvvm.R
+
+object DataJK {
+    val listJK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
---
+++ app/src/main/java/com/example/praktikummvvm/model/Mahasiswa.kt
@@ -0,0 +1,7 @@
+package com.example.praktikummvvm.model
+
+data class Mahasiswa(
+    val nama : String = "",
+    val gender : String = "",
+    val alamat : String = ""
+)
